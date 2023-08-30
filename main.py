c03f4e4d316782585d9471b6de18b7402bfb512a
-LilNug- <dk15022007@gmail.com>
2023-08-30 10:34:35 +0530

    trying to make db manager
    figured out the basics

--- main.py
+++ main.py
@@ -3,35 +3,34 @@ import flask as f
 from simulation_functions import *
 
 
+host='localhost'
+user='root'
+password='5120'
+database='school'
 
 
 app = f.Flask(__name__, template_folder='', static_folder='')
 app.config.update(TEMPLATES_AUTO_RELOAD=True)
 
-prices = {'hdfc':1232,'uco':11,'zomato':61,'hindustan_uni':2500,'cipla':920,'tata':450}
-
-@app.route('/', methods=['POST','GET'])
-def home():
-    if f.request.method=='POST':
-        buybtn=f.request.form.get('buy')
-        sellbtn=f.request.form.get('sell')
-        admno= f.request.form["admno"]
-        comp=f.request.form["company"].lower()
-        shares=int(f.request.form["noshares"])
-        if buybtn:
-            insert(admno,comp,shares,prices)
-        if sellbtn:
-            delete(admno,comp,shares,prices)
-    db=mysql.connector.connect(host='localhost', user='root', password='', database='workex')
-    cur = db.cursor()
-    cur.execute('select * from students;')
+@app.route('/')
+def index():
+    cn=mysql.connector.connect(host=host, user=user, password=password,database=database)
+    cur=cn.cursor()
+    cur.execute('show tables;')
     data=cur.fetchall()
-    db.close()
+    cn.close()
     cur.close()
     return f.render_template('index.html', data=data)
-
-
-
+    
+    
+@app.route('/table', methods=['POST'])
+def table():
+    cn=mysql.connector.connect(host=host, user=user, password=password,database=database)
+    cur=cn.cursor()
+    cur.execute(f"select * from {f.request.form['table']}")
+    data=cur.fetchall()
+    return f.render_template('sim_page.html', data=data)
 
 if __name__ == "__main__":
+    
     app.run(debug=True)
